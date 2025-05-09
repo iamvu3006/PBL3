a6da0aee8126f812dedce7ba8aea34a0fac0a7b1
Tusk-2005 <Khongtu270405@gmail.com>
2025-05-10 00:57:56 +0700

    chưa sửa product nhưng test đc auth , đăng nhập xog ko làm j được

--- src/main/java/com/pbl3/ecommerce/controller/AuthController.java
+++ src/main/java/com/pbl3/ecommerce/controller/AuthController.java
@@ -1,13 +1,17 @@
 package com.pbl3.ecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.ResponseBody;
+
 import com.pbl3.ecommerce.dto.AuthResponse;
 import com.pbl3.ecommerce.dto.LoginRequest;
 import com.pbl3.ecommerce.dto.RegisterRequest;
 import com.pbl3.ecommerce.service.AuthService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
 
 import jakarta.servlet.http.HttpSession;
 
@@ -90,4 +94,5 @@ public class AuthController {
             return ResponseEntity.ok(new AuthResponse(false, "Chưa đăng nhập"));
         }
     }
+    
 }
