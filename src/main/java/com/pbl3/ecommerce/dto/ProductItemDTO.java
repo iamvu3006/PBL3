a10f85420f1f66f7e3721bc9acb418b6347994f7
DaoQuan03 <daoquan20032005@gmail.com>
2025-05-10 21:43:21 +0700

    tiep tuc la xu ly exception

--- src/main/java/com/pbl3/ecommerce/dto/ProductItemDTO.java
+++ src/main/java/com/pbl3/ecommerce/dto/ProductItemDTO.java
@@ -1,35 +1,113 @@
 package com.pbl3.ecommerce.dto;
 
-
 import com.pbl3.ecommerce.entity.ProductItem;
-import jakarta.persistence.criteria.CriteriaBuilder;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ProductItemDTO {
+    private static final Logger logger = LoggerFactory.getLogger(ProductItemDTO.class);
+
     private Integer productitemId;
     private Integer clientId;
     private Integer sellCategoryId;
+    private String clientPhoneNumber;
     private String colorName;
     private String configurationHardDrive;
     private String productType;
-    private Double configurationInchs;
+    private String Inchs;
     private Integer productRam;
     private Integer internalMemory;
     private String brandName;
     private String tafiffPakageName;
 
+    public ProductItemDTO(ProductItem item) {
+        try {
+            this.productitemId = item.getProductItemID();
 
-    public ProductItemDTO(ProductItem item){
-        this.productitemId = item.getProductItemID();
-        this.clientId = item.getAbClient().getClientID();
-        this.sellCategoryId = item.getSellCategory().getSellCategoryID();
-        this.brandName = item.getBrand().getBrandName();
-        this.colorName = item.getColor().getColorName();
-        this.configurationHardDrive = item.getConfiguration().getHardDrive().toString();
-        this.configurationInchs = item.getConfiguration().getInchs();
-        this.productType = item.getProducttype().toString();
-        this.productRam = item.getConfiguration().getGbMemory().getRam();
-        this.internalMemory = item.getConfiguration().getGbMemory().getInternalMemory();
-        this.tafiffPakageName = item.getTariffiPackage().getPackageName();
+            if (item.getAbClient() != null) {
+                this.clientId = item.getAbClient().getClientID();
+
+                if (item.getAbClient().getClientPhoneNumber() != null) {
+                    this.clientPhoneNumber = item.getAbClient().getClientPhoneNumber();
+                } else {
+                    logger.warn("Số điện thoại không có tại client có ID: {}", item.getAbClient().getClientID());
+                }
+            } else {
+                logger.warn("AbClient là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getSellCategory() != null) {
+                this.sellCategoryId = item.getSellCategory().getSellCategoryID();
+            } else {
+                logger.warn("SellCategory là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getBrand() != null) {
+                this.brandName = item.getBrand().getBrandName();
+            } else {
+                logger.warn("Brand là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getColor() != null) {
+                this.colorName = item.getColor();
+            } else {
+                logger.warn("Color là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getRam() != null) {
+                this.productRam = item.getRam();
+            } else {
+                logger.warn("Ram là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getInchs() != null) {
+                this.Inchs = item.getInchs();
+            } else {
+                logger.warn("Inchs là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getProducttype() != null) {
+                this.productType = item.getProducttype().toString();
+            } else {
+                logger.warn("ProductType là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getTariffiPackage() != null) {
+                this.tafiffPakageName = item.getTariffiPackage().getPackageName();
+            } else {
+                logger.warn("TariffiPackage là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getHardDriveType() != null) {
+                this.configurationHardDrive = item.getHardDriveType();
+            } else {
+                logger.warn("ConfigurationHardDrive là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+            if (item.getInternalmemory() != null) {
+                this.internalMemory = item.getInternalmemory();
+            } else {
+                logger.warn("InternalMemory là null cho ProductItem có ID: {}", item.getProductItemID());
+            }
+
+        } catch (Exception e) {
+            logger.error("Lỗi khi tạo ProductItemDTO từ entity: {}", e.getMessage(), e);
+            throw e;
+        }
+    }
+
+
+    // Getters
+    public Integer getProductitemId() {
+        return productitemId;
+    }
+
+    public Integer getClientId() {
+        return clientId;
+    }
+
+    public Integer getSellCategoryId() {
+        return sellCategoryId;
     }
 
     public String getColorName() {
@@ -44,8 +122,8 @@ public class ProductItemDTO {
         return productType;
     }
 
-    public Double getConfigurationInchs() {
-        return configurationInchs;
+    public String getInchs() {
+        return Inchs;
     }
 
     public Integer getProductRam() {
@@ -63,6 +141,36 @@ public class ProductItemDTO {
     public String getTafiffPakageName() {
         return tafiffPakageName;
     }
+
+    public void setColorName(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public void setConfigurationHardDrive(String configurationHardDrive) {
+        this.configurationHardDrive = configurationHardDrive;
+    }
+
+    public void setProductType(String productType) {
+        this.productType = productType;
+    }
+
+    public void setInchs(String inchs) {
+        this.Inchs = inchs;
+    }
+
+    public void setProductRam(Integer productRam) {
+        this.productRam = productRam;
+    }
+
+    public void setInternalMemory(Integer internalMemory) {
+        this.internalMemory = internalMemory;
+    }
+
+    public void setBrandName(String brandName) {
+        this.brandName = brandName;
+    }
+
+    public void setTafiffPakageName(String tafiffPakageName) {
+        this.tafiffPakageName = tafiffPakageName;
+    }
 }
-
-
