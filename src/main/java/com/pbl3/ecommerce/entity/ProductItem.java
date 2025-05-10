6164ef46a2c0949c2742d29575dd40174b492ca8
DaoQuan03 <daoquan20032005@gmail.com>
2025-05-10 07:20:54 +0700

    xoa producttype + them lien ket voi abclient

--- src/main/java/com/pbl3/ecommerce/entity/ProductItem.java
+++ src/main/java/com/pbl3/ecommerce/entity/ProductItem.java
@@ -1,28 +1,29 @@
 package com.pbl3.ecommerce.entity;
-import java.util.ArrayList;
-import java.util.List;
+import jakarta.persistence.*;
 
-import jakarta.persistence.CascadeType;
-import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
-import jakarta.persistence.OneToMany;
-import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "productitem")
 public class ProductItem {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer productItemID;
 
     @Enumerated(EnumType.STRING)
-    private ProductRole productRole;
+    private ProductType producttype;
+
+    public ProductType getProducttype() {
+        return producttype;
+    }
+
+    public void setProducttype(ProductType producttype) {
+        this.producttype = producttype;
+    }
+
+    @ManyToOne
+    @JoinColumn(name = "Clientid")
+    private AbClient abclient;
 
     @ManyToOne
     @JoinColumn(name = "ColorID")
@@ -43,37 +44,17 @@ public class ProductItem {
     @ManyToOne
     @JoinColumn(name = "SellCategoryID")
     private SellCategory sellCategory;
-     @OneToMany(mappedBy = "productItem", cascade = CascadeType.ALL, orphanRemoval = true)
-    private List<Descripted> descripteds = new ArrayList<>();
-
-    
-    public List<Descripted> getDescripteds() {
-        return descripteds;
-    }
-    public void setDescripteds(List<Descripted> descripteds) {
-        this.descripteds = descripteds;
-    }
-
-    public enum ProductRole {
-        Seller, Buyer
-    }
 
     // Getters & Setters
-
+    public enum ProductType{
+        Laptop, Phone
+    }
     public Integer getProductItemID() {
         return productItemID;
     }
 
-    public void setProductItemID(Integer productItemID) {
+    private void setProductItemID(Integer productItemID) {
         this.productItemID = productItemID;
-    }
-
-    public ProductRole getProductRole() {
-        return productRole;
-    }
-
-    public void setProductRole(ProductRole productRole) {
-        this.productRole = productRole;
     }
 
     public Color getColor() {
@@ -112,7 +93,15 @@ public class ProductItem {
         return sellCategory;
     }
 
-    public void setSellCategory(SellCategory sellCategory) {
+    private void setSellCategory(SellCategory sellCategory) {
         this.sellCategory = sellCategory;
+    }
+
+    public AbClient getAbClient() {
+        return abclient;
+    }
+
+    private void setAbClient(AbClient abClient) {
+        this.abclient = abClient;
     }
 }
