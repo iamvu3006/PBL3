1df19d16d0249b7b1a52fbcd88104aec47c44700
ba vu <lexuanbavu@gmail.com>
2025-05-24 16:21:50 +0700

    Thêm tính năng "Đăng xuất" sau khi đăng nhập và tính năng giỏ hàng đã có thể truy cập được. Nhưng còn lỗi ở tính năng "Đăng tin"

--- src/main/java/com/pbl3/ecommerce/controller/PushProductItemController.java
+++ src/main/java/com/pbl3/ecommerce/controller/PushProductItemController.java
@@ -5,20 +5,45 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.pbl3.ecommerce.dto.PushProductItemDTO;
 import com.pbl3.ecommerce.service.PushProductItemService;
+import com.pbl3.ecommerce.repository.BrandRepository;
+import com.pbl3.ecommerce.repository.SellCategoryRepository;
+import com.pbl3.ecommerce.repository.TariffiPackageRepository;
 
-@RestController
+@Controller
 @RequestMapping("/api/products")
 public class PushProductItemController {
 
     @Autowired
     private PushProductItemService productItemService;
 
+    @Autowired
+    private BrandRepository brandRepo;
+    @Autowired
+    private SellCategoryRepository sellCatRepo;
+    @Autowired
+    private TariffiPackageRepository tariffRepo;
+
     @PostMapping("/create")
+    @ResponseBody
     public String createProduct(@RequestBody PushProductItemDTO dto) throws Exception {
         productItemService.createProductItem(dto);
         return "Đăng bán sản phẩm thành công!";
+    }
+
+    @GetMapping("/create")
+    public String showCreateForm(Model model) {
+        model.addAttribute("brands", brandRepo.findAll());
+        model.addAttribute("categories", sellCatRepo.findAll());
+        model.addAttribute("tariffs", tariffRepo.findAll());
+        // Nếu có clientId, bạn có thể lấy từ session hoặc security context
+        // model.addAttribute("clientId", ...);
+        return "create";
     }
 }
