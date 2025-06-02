8977eb3fc4dc825a39bcfb8af2622507702edfd9
DaoQuan03 <daoquan20032005@gmail.com>
2025-06-02 22:34:16 +0700

    chinh sua lai quan he giua clien va wishlistcategory

--- src/main/java/com/pbl3/ecommerce/entity/AbClient.java
+++ src/main/java/com/pbl3/ecommerce/entity/AbClient.java
@@ -1,6 +1,9 @@
 package com.pbl3.ecommerce.entity;
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 @Table(name = "abclient")
 public class AbClient {
@@ -21,7 +24,39 @@ public class AbClient {
 
     private String clientAdress;
 
+    @OneToMany(mappedBy = "client", cascade = CascadeType.ALL)
+    private List<ProductCategoryClient> productCategoryClients = new ArrayList<>();
+
+    @OneToOne(cascade = CascadeType.ALL, optional = false)
+    @JoinColumn(name = "wishlistid")
+    private WishListCategory wishListCategory;
+
     // getters & setters
+
+    public WishListCategory getWishListCategory() {
+        return wishListCategory;
+    }
+
+    public void setWishListCategory(WishListCategory wishListCategory) {
+        if(wishListCategory == null){
+            if(this.wishListCategory != null)
+                this.wishListCategory.setAbClient(null); //ngat ket noi tu wishlist toi client neu wishlist khong con ton tai nua
+        } else {
+            wishListCategory.setAbClient(this);     //dam bao wishlist co the tham chieu nguoc lai
+
+            if(this.wishListCategory != null && this.wishListCategory != wishListCategory) //trong truong hop client da co wishlish khac
+                this.wishListCategory.setAbClient(null); //ngat ket noi voi wishlist cu
+        }
+        this.wishListCategory = wishListCategory;
+    }
+
+    public List<ProductCategoryClient> getProductCategoryClients() {
+        return productCategoryClients;
+    }
+
+    public void setProductCategoryClients(List<ProductCategoryClient> productCategoryClients) {
+        this.productCategoryClients = productCategoryClients;
+    }
 
     public Integer getClientID() {
         return clientID;
