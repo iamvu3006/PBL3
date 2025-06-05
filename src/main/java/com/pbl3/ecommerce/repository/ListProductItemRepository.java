a5edbc22b03c96749394c8b59d83748025d8cee1
DaoQuan03 <daoquan20032005@gmail.com>
2025-06-05 09:19:45 +0700

    query lay san pham theo thuong hieu hoac gia hoac ca 2

--- src/main/java/com/pbl3/ecommerce/repository/ListProductItemRepository.java
+++ src/main/java/com/pbl3/ecommerce/repository/ListProductItemRepository.java
@@ -1,13 +1,26 @@
-package com.pbl3.ecommerce.repository;
+    package com.pbl3.ecommerce.repository;
 
-import com.pbl3.ecommerce.entity.ProductItem;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
+    import com.pbl3.ecommerce.entity.ProductItem;
+    import org.springframework.data.jpa.repository.JpaRepository;
+    import org.springframework.data.jpa.repository.Query;
+    import org.springframework.data.repository.query.Param;
 
-import java.util.List;
+    import java.util.List;
 
-public interface ListProductItemRepository extends JpaRepository<ProductItem, Integer> {
-    @Query("SELECT p FROM ProductItem p JOIN p.productItemCategories pic JOIN pic.category c WHERE c.categoryID = :categoryId")
-    List<ProductItem> findByCategoryId(@Param("categoryId") Integer categoryId);
-}
+    public interface ListProductItemRepository extends JpaRepository<ProductItem, Integer> {
+        @Query("SELECT p FROM ProductItem p JOIN p.productItemCategories pic JOIN pic.category c WHERE c.categoryID = :categoryId")
+        List<ProductItem> findByCategoryId(@Param("categoryId") Integer categoryId);
+
+        @Query("""
+                SELECT p FROM ProductItem p
+                WHERE (:brand IS NULL OR p.Brandid.brandName = :brand)
+                AND (:minPrice IS NULL OR p.price >= :minPrice)
+                AND (:maxPrice IS NULL OR p.price <= :maxPrice)
+                """)
+        List<ProductItem> filterProducts(
+                @Param("brand") String brand,
+                @Param("minPrice") Double minPrice,
+                @Param("maxPrice") Double maxPrice
+        );
+
+    }
